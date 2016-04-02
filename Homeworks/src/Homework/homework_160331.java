80629b777f64f26cdb1b521dfb527d1947fb6c20
Lemis <Lemis@terminal10-home>
2016-04-02 12:30:50 +0300

    Homework Done

---
+++ Homeworks/src/Homework/homework_160331.java
@@ -0,0 +1,12 @@
+package Homework;
+
+//Homework about income taxing
+
+public class homework_160331 {
+	public static void main(String[] args) {
+		float incomeSum = 26700f; //income sum
+		System.out.println("Income: " + incomeSum); //full income
+		System.out.println("On hand: " + incomeSum * 0.87); //income after tax
+		System.out.println("Tax: " + incomeSum * 0.13); //13% of tax
+	}
+}
