98573c203e940253340d14e8b4b6275ea21597b2
lemis <lemis@lemis-100NZC>
2016-04-07 14:56:43 +0300

    Homework 160405

---
+++ Homeworks/src/Homework/Homework160405.java
@@ -0,0 +1,19 @@
+package Homework;
+
+public class Homework160405 {
+
+	public static void main(String[] args) {
+		//homework about ЯUЫЫIAИ ВОДКА ДЯIИKIИG, КАМРАД!
+		int x = Integer.parseInt(args[0]);
+		
+		if (x == 2) {
+			System.out.println("Много!");
+		} else if (x == 5) {
+			System.out.println("Мало!");
+		} else if (x == 1) {
+			System.out.println("В самый раз!");
+		} else {
+			System.out.println("Что-то не то, '" + x + "' тут точно никаким боком!");
+		}		
+	}
+}
